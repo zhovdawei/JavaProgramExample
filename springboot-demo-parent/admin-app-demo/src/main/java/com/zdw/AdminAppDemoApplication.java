4bc9ff5db1eee54cc0f6ac63f2c0fbb9e841a475
zhovdawei <zdw0607@gmail.com>
2019-12-28 22:15:47 +0800

    springboot demo 工程结构

---
+++ springboot-demo-parent/admin-app-demo/src/main/java/com/zdw/AdminAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminAppDemoApplication.class, args);
+    }
+
+}
